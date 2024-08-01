1d6efc2c02c067898dc9660a2c7a0fccece02ba5
root <mathewkoech>
2024-08-01 18:03:23 +0000

    variables defination

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This script defines and annotates several variables with specific values.
+
+Variables defined:
+- a: An integer with a value of 1.
+- pi: A float with a value of 3.14, representing the mathematical constant pi.
+- i_understand_annotations: A boolean set to True,
+indicating an understanding of type annotations.
+- school: A string with the value "Holberton", representing a name.
+"""
+
+# Define and annotate variables
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

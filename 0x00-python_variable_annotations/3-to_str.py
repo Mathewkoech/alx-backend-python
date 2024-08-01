c001d9cd21dca335b3e518eebffeca35b6c420ab
root <mathewkoech>
2024-08-01 17:52:54 +0000

    function that converts input float as string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module that takes float as input and converts its return value to a string
+"""
+
+def to_str(n: float) -> str:
+    """
+    Convert a float number to its string representation.
+
+    Parameters:
+    n (float): The float number to be converted.
+
+    Returns:
+    str: The string representation of the float number.
+    """
+    return str(n)
